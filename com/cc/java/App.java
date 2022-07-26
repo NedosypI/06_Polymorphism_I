aba383f87b8137f00316bc2857fd98c4659f8f73
NedosypI <irinanedosyp@gmx.de>
2022-07-26 11:10:13 +0200

    Superklasse / Subklassen /Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,37 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+// Da diese typen von Honeybee überschrieben werden, dürfen wir dies ändern 
+// Worker worker = new Worker();
+// Queen queen = new Queen();
+// Drone drone = new Drone();
+
+/* Honeybee = Referenztyp
+ * worker = Referenzvariable
+ * new Worker = Konstruktor fault 
+ */
+Honeybee worker = new Worker();
+Honeybee queen = new Queen();
+Honeybee drone = new Drone();
+
+
+output(worker.doYourJob());
+output(queen.doYourJob());
+output(drone.doYourJob());
+
+// Das sollte eigentlich nicht gehen 
+Honeybee bee = new Honeybee();
+output(bee.doYourJob());
+
+
+output("--------------------------");
+
+output(worker.fly());
+output(queen.fly());
+output(drone.fly());
+
+
     }
 
     public static void output(String outputStr) {
